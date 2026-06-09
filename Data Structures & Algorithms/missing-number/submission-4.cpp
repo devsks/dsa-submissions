bc6507c55c37f49d02984a0a2f71e4a2bd753080
Santosh Kumar Shaw <coolnicesantosh@gmail.com>
2026-06-09 20:07:58 +0530

    Add: missing-number - submission-4

---
+++ Data Structures & Algorithms/missing-number/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int totalXor=0, actualXor=0;
+        for(int i=1;i<=nums.size();++i)
+            totalXor^=i;
+
+        for(int &num:nums)
+            actualXor^=num;
+
+        return totalXor^actualXor;
+    }
+};
